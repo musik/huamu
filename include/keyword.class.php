4b7ddca1fcc2123ea300c82f468b22e794e08fab
musik <cgicn@qq.com>
2014-05-14 13:47:56 +0800

    keyword index

---
+++ include/keyword.class.php
@@ -0,0 +1,60 @@
+<?php
+class keyword {
+	var $db;
+	var $table;
+
+	function keyword() {
+		global $db, $DT_PRE;
+		$this->table = $DT_PRE.'keyword';
+		$this->db = &$db;
+	}
+
+	function get_list($condition, $order) {
+		global $pages, $page, $pagesize, $offset, $pagesize;
+		$pages = pages($this->db->count($this->table, $condition), $page, $pagesize);
+		$lists = array();
+		$result = $this->db->query("SELECT * FROM {$this->table} WHERE $condition ORDER BY $order LIMIT $offset,$pagesize");
+		while($r = $this->db->fetch_array($result)) {
+			$lists[] = $r;
+		}
+		return $lists;
+	}
+
+	function update($post) {
+		$this->add($post[0]);
+		unset($post[0]);
+		foreach($post as $k=>$v) {
+			if(isset($v['delete'])) {
+				$this->delete($k);
+				unset($post[$k]);
+			}
+		}
+		$this->edit($post);
+	}
+
+	function add($post) {
+		global $DT_TIME;
+		if(!$post['word']) return false;
+		$post['status'] = $post['status'] == 3 ? 3 : 2;
+		$this->db->query("INSERT INTO {$this->table} (moduleid,word,keyword,letter,items,total_search,month_search,week_search,today_search,updatetime,status) VALUES('$post[moduleid]','$post[word]','$post[keyword]','$post[letter]','$post[items]','$post[total_search]','$post[month_search]','$post[week_search]','$post[today_search]','$DT_TIME', '$post[status]')");
+	}
+
+	function edit($post) {
+		foreach($post as $k=>$v) {
+			if(!$v['word']) continue;
+			$v['status'] = $v['status'] == 3 ? 3 : 2;
+			$this->db->query("UPDATE {$this->table} SET word='$v[word]',keyword='$v[keyword]',letter='$v[letter]',total_search='$v[total_search]',month_search='$v[month_search]',week_search='$v[week_search]',today_search='$v[today_search]',status='$v[status]' WHERE itemid='$k'");
+		}
+	}
+
+	function delete($itemid) {
+		$this->db->query("DELETE FROM {$this->table} WHERE itemid=$itemid");
+	}
+  function check_install(){
+    $r = $this->db->get_one("show columns from $this->table like 'ali_cat'");
+    if($r) return false;
+    $this->db->query("alter table $this->table add column ali_cat integer(11)");
+    return true;
+  }
+}
+?>
---
+++ module/sell/ki.inc.php
@@ -0,0 +1,25 @@
+<?php 
+defined('IN_DESTOON') or exit('Access Denied');
+if($DT_BOT || $_POST) dhttp(403);
+require DT_ROOT.'/module/'.$module.'/common.inc.php';
+if(!check_group($_groupid, $MOD['group_search'])) include load('403.inc');
+require DT_ROOT.'/include/post.func.php';
+require DT_ROOT.'/include/keyword.class.php';
+include load('search.lang');
+$do = new keyword();
+$sorder  = array('结果排序方式', '总搜索量降序', '总搜索量升序', '本月搜索降序', '本月搜索升序', '本周搜索降序', '本周搜索升序', '今日搜索降序', '今日搜索升序', '信息数量降序', '信息数量升序', '更新时间降序', '更新时间升序');
+$dorder  = array('itemid DESC', 'total_search DESC', 'total_search ASC', 'month_search DESC', 'month_search ASC', 'week_search DESC', 'week_search ASC', 'today_search DESC', 'today_search ASC', 'items DESC', 'items ASC', 'updatetime DESC', 'updatetime ASC');
+isset($order) && isset($dorder[$order]) or $order = 0;
+$order_select  = dselect($sorder, 'order', '', $order);
+$status = isset($status) ? intval($status) : 3;
+$condition = "status=$status";
+if($keyword) $condition .= " AND keyword LIKE '%$keyword%'";
+if($mid) $condition .= " AND moduleid=$mid";
+$pagesize = 200;
+$lists = $do->get_list($condition, $dorder[$order]);
+$pages = str_replace('sell/ki-htm-page-1.html','ki',$pages);
+$seo_file = 'ki';
+include DT_ROOT.'/include/seo.inc.php';
+$seo_title = "热门关键词".$seo_delimiter.$seo_sitename;
+include template('ki', $module);
+?>
